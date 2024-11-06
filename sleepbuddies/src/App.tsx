9b9c08b87e7ec60bfd5e70f734af58c4ceca3099
Tom Zhang <treez.zhang@gmail.com>
2024-11-05 16:30:31 -0800

    set up homepage
    Sets up home page and router-dom

--- sleepbuddies/src/App.tsx
+++ sleepbuddies/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./css/App.css";
 
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./elements/pages/home";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <h1>Sleep Buddies</h1>
+          </header>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+          </Routes>
+      </div>
   );
 }
 
--- sleepbuddies/src/elements/pages/home.tsx
+++ sleepbuddies/src/elements/pages/home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Dashboard = () => {
+export const Home = () => {
     return (
         <>
             <h1>Home</h1>
         </>
     );
 };
-
-export default Dashboard;
