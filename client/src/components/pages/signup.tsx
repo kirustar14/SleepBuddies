a7e472df3269e40a20f7613e42edd33f5654578c
JingtingL <jingtingliu123@gmail.com>
2024-11-07 15:13:35 -0800

    start screen is the welcome page, added login and start page

---
+++ client/src/components/pages/signup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const signup = () => {
+    return (
+        <>
+            <h1>SignUp</h1>
+        </>
+    );
+};
+
+export default signup;
---
+++ client/src/components/pages/welcome.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "../elements/Login";
+import Signup from "../elements/Signup";
+const welcome = () => {
+    return (
+        <>
+            <h1>Welcome to Sleep Buddies </h1>
+            <div className="welcome_page-div">
+                <Login />
+                <Signup />
+            </div>
+        </>
+    );
+};
+
+export default welcome;
