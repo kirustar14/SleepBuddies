f0aaf6a8342a7d4fbc13c025ead54852ff190f93
joshuamkchoi1103 <jmc008@ucsd.edu>
2024-11-06 23:00:11 -0800

    updated statistics title

--- client/src/components/elements/SleepAnalytics/Statistics.tsx
+++ client/src/components/elements/SleepAnalytics/Statistics.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export const Statistics = () => {
     return (
         <>
+            <h2>Sleeping Data</h2>
             <h3>Average Hours Slept this Week: 10</h3>
         </>
     );
