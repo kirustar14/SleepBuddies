9836731ea7030739b71566a6d6b65abeb23fbd28
jut008 <122562428+jut008@users.noreply.github.com>
2024-11-08 17:48:51 -0800

    updated 2: welcome, login, sign up pages
    forgot to include welcome.css, and fixed welcome screen buttons to text buttons instead

--- client/src/components/elements/Signup.tsx
+++ client/src/components/elements/Signup.tsx
@@ -1,11 +1,11 @@
-import Image from "../../assets/clock.svg";
+import React from "react";
 import {Link} from "react-router-dom";
 
 const Signup = () => {
     return (
         <Link className="clickable" to="/signup">
             <div className="applet">
-                <img className="image" src={Image} alt={"Login icon"}/>
+                <p className="button-text">Sign Up</p>
             </div>
         </Link>
     );
