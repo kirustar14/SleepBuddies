b5e73226110c29fb40bf082be544a33c6aa0c686
Tom Zhang <treez.zhang@gmail.com>
2024-11-14 16:12:25 -0800

    sets up db
    installs sqlite and sets up database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import initDB from "./createTable";
 
 const express = require("express");
 const cors = require("cors");
@@ -14,10 +15,13 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
+// Initialize the database and start the server
+(async () => { const db = await initDB();
 
-// Root endpoint to get test if the server is running
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send({ "data": "Hello, TypeScript Express!" });
-});
+  // Root endpoint to get test if the server is running
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).send({ "data": "Hello, TypeScript Express!" });
+  });
+})();
 
 export{}
