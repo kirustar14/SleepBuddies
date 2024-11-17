dea0543548f07d9f33763ad721469fdbce3826bc
jut008 <122562428+jut008@users.noreply.github.com>
2024-11-17 14:38:00 -0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,6 @@ import welcome from './components/pages/welcome';
 import Music from "./components/pages/music";
 
 function App() {
-<<<<<<< Updated upstream
     return (
         <div className="App">
             <header className="App-header">
@@ -32,25 +31,5 @@ function App() {
             </Routes>
         </div>
     );
-=======
-  return (
-      <div className="App">
-          <header className="App-header">
-              <h1>Sleep Buddies</h1>
-          </header>
-          <Routes>
-              <Route path="/" Component={welcome}/>
-              <Route path="/login" Component={Login}/>
-              <Route path="/signup" Component={signup}/>
-              <Route path="/home" Component={Home}/>
-              <Route path="/sleep" Component={Sleep}/>
-              <Route path="/meditation" Component={Meditation}/>
-              <Route path="/alarm" Component={Alarm}/>
-              <Route path="/music" Component={Music}/>
-          </Routes>
-      </div>
-  );
->>>>>>> Stashed changes
-}
-
+};
 export default App;
