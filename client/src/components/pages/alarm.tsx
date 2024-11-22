65a187cf9dfcf6671c917aafce1cf3ae3581ad81
JingtingL <jingtingliu123@gmail.com>
2024-11-22 02:08:10 -0800

    New Alarms Page Setup
    Making an alarm list to keep track of alarms

--- client/src/components/pages/alarm.tsx
+++ client/src/components/pages/alarm.tsx
@@ -2,11 +2,30 @@ import React from "react";
 import "../../css/alarm.css";
 
 const Alarm = () => {
+  const alarms = [
+    "07:00 AM - Morning Alarm",
+    "12:00 PM - Lunch Alarm",
+    "05:00 PM - Evening Workout Alarm",
+  ];
+
   return (
-    <div className="alarm-icon-container" style={{ marginTop: '10px' }}>
-      <h1 className="alarm-page-title">Alarms</h1>
+    <div className="alarm-page">
+      <div className="alarm-header">
+        <h1 className="alarm-page-title">Alarms</h1>
+        <button className="add-alarm-button">+</button>
+      </div>
+      <div className="alarm-list">
+        {alarms.map((alarm, index) => (
+          <div key={index} className="alarm-item">
+            {alarm}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Alarm;
+
+
+
